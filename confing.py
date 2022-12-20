086fbf97f887e0e305dc81215c324f7674f7c45c
Sanjar0908 <beektesan@gmail.com>
2022-12-20 16:41:07 +0600

    hw3

--- confing.py
+++ confing.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Dispatcher
 from decouple import config
-
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 TOKEN = config("TOKEN")
-
+storage = MemoryStorage()
 bot = Bot(TOKEN)
-dp = Dispatcher(bot=bot)
+dp = Dispatcher(bot=bot, storage=storage)
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from aiogram.utils import executor
 from confing import dp
 import logging
-from handleers import client, colback, extra, admin
+from handleers import client, colback, extra, admin,fsmAdminMentor
 
+fsmAdminMentor.register_handlers_fsm_anketa(dp)
 client.regiter_handler_client(dp)
 colback.register_handler_callback(dp)
 admin.register_handler_admin(dp)
